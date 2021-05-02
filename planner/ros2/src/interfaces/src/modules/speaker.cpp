f699958a1efdbc0861a04ea24ff02591b65f5afb
Voldivh <eloyabmfcv@gmail.com>
2021-05-02 08:18:16 -0500

    [FEAT]: Fix C++ P4
    Fixed the default song. I was inputing "track2.wav" instead of "2.wav" on the default case.

--- planner/ros2/src/interfaces/src/modules/speaker.cpp
+++ planner/ros2/src/interfaces/src/modules/speaker.cpp
@@ -107,7 +107,7 @@ void Speaker::speakerCb(const std_msgs::msg::Int8::SharedPtr msg)
         ********************************************/
         else 
         {
-            readfd = open((m_path + "track2.wav").c_str(), O_RDONLY);
+            readfd = open((m_path + "2.wav").c_str(), O_RDONLY);
             status = pthread_create(&pthread_id, NULL, (THREADFUNCPTR)&Speaker::PlaySound, this);
         }
         /********************************************
